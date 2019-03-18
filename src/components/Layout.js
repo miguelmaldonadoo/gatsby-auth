638733faa221fae3399f875bccb2f62c6284a3e3
Dipesh Wagle <miguelmaldonadoo>
2019-03-18 11:26:41 +0545

    use auth

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -65,11 +65,11 @@ class Layout extends Component {
             <Menu auth={!authenticated?{title:'Login',action:this.login}:{title:'Logout',action:this.logout}}/>
             
             { children }
-            {/* { (authenticated || this.props.noAuth) && children}
+            { (authenticated || this.props.noAuth) && children}
             {(!authenticated && !this.props.noAuth )&& <>
               <br/>
               <h1 style={{margin:'auto',fontSize:40}}>You need to login to see the content</h1></>
-            } */}
+            }
           </div>
           <Footer />
         </>
